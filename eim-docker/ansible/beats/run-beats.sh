c929991736f9ed297d5c8bf6bab810d0d04b9250
david-rojo <david.rojoa@atos.net>
2017-08-13 21:43:06 +0200

    old folder deleted

--- eim-docker/ansible/beats/run-beats.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-ansible-playbook /var/ansible/beats/ansible-beats-master/playbook-beats-output-logstash.yml --extra-vars "ansible_become_pass=elastest"
--- eim-docker/ansible/ssh/run-ssh.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-ansible-playbook /var/ansible/ssh/playbook-ssh.yml
-
