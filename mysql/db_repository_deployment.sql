67191a9c6cea43b7ad0d854ff0ab8b434595f170
David Rojo <david.rojoa@atos.net>
2018-02-02 17:42:27 +0100

    create db script added

---
+++ mysql/db_repository_deployment.sql
@@ -0,0 +1,35 @@
+create database if not exists `eim`;
+
+USE `eim`;
+
+
+/*Table structure for table `agent` */
+
+DROP TABLE IF EXISTS `agent`;
+
+CREATE TABLE `agent` (
+	  `agent_id` varchar(255) NOT NULL,
+	  `host` varchar(255) NOT NULL,
+	  `monitored` tinyint(1) NOT NULL,
+	  `logstash_ip` varchar(255) NOT NULL,
+	  `logstash_port` varchar(255) NOT NULL,
+	  PRIMARY KEY (`agentId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Table structure for table `agentconfiguration` */
+
+DROP TABLE IF EXISTS `agent_configuration`;
+
+CREATE TABLE `agent_configuration` (
+	  `agent_id` varchar(255) NOT NULL,
+	  `exec` varchar(255) NOT NULL,
+	  `component` varchar(255) NOT NULL,
+	  `packetbeat_stream` varchar(255) NOT NULL,
+	  `topbeat_stream` varchar(255) NOT NULL,
+	  `filebeat_stream` varchar(255) NOT NULL,
+	  `filebeat_paths` text NOT NULL,
+	  PRIMARY KEY (`agentId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
