5b315a4f8322b1fa6e7f7eeddcf59953c6920b4e
David Rojo <david-rojo@users.noreply.github.com>
2018-02-02 19:49:55 +0100

    fix folder name

--- mysql/2-setup.sh
+++ mysql/2-setup.sh
@@ -3,4 +3,4 @@ set -e
 set -x
 
 # EIM schema
-mysql -u elastest -p'elastest' eim < /eim/repository/db_repository_deployment.sql
+mysql -u elastest -p'elastest' eim < /elastest/repository/db_repository_deployment.sql
