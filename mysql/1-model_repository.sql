14641567d7b6642258ffb663792a98aff327d074
David Rojo <david.rojoa@atos.net>
2018-02-10 02:27:27 +0100

    added env var for eim server

--- mysql/1-model_repository.sql
+++ mysql/1-model_repository.sql
@@ -1,4 +1,6 @@
 create database eim;
 GRANT ALL PRIVILEGES ON eim.* TO 'elastest'@'localhost' identified by 'elastest';
 grant usage on eim.* to 'elastest'@'localhost' identified by 'elastest';
+GRANT ALL PRIVILEGES ON eim.* TO 'elastest'@'ET_EIM_SERVER' identified by 'elastest';
+grant usage on eim.* to 'elastest'@'ET_EIM_SERVER' identified by 'elastest';
 flush privileges;
