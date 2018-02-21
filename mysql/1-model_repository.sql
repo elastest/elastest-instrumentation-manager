a97bd298de06aa8c128dc0134a077e1a0d336622
David Rojo <david-rojo@users.noreply.github.com>
2018-02-21 15:00:32 +0100

    added remote connection access

--- mysql/1-model_repository.sql
+++ mysql/1-model_repository.sql
@@ -1,6 +1,11 @@
 create database eim;
-GRANT ALL PRIVILEGES ON eim.* TO 'elastest'@'localhost' identified by 'elastest';
-grant usage on eim.* to 'elastest'@'localhost' identified by 'elastest';
-GRANT ALL PRIVILEGES ON eim.* TO 'elastest'@'##EIM_SERVER##' identified by 'elastest';
-grant usage on eim.* to 'elastest'@'##EIM_SERVER##' identified by 'elastest';
+/* GRANT ALL PRIVILEGES ON eim.* TO 'elastest'@'localhost' identified by 'elastest';
+*  grant usage on eim.* to 'elastest'@'localhost' identified by 'elastest';
+*  GRANT ALL PRIVILEGES ON eim.* TO 'elastest'@'##EIM_SERVER##' identified by 'elastest';
+*  grant usage on eim.* to 'elastest'@'##EIM_SERVER##' identified by 'elastest';
+*/
+
+GRANT ALL PRIVILEGES ON *.* TO 'elastest'@'%' identified by 'elastest';
+grant usage on *.* to 'elastest'@'%' identified by 'elastest';
+
 flush privileges;
