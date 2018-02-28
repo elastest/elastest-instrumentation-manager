33e95ce073ef3d36dc65f89b93ec123134b635ae
david-rojo <davidrojoatos@gmail.com>
2018-02-28 14:59:19 +0100

    automatized manual steps, ready to use image for eim testing

--- sut/entrypoint.sh
+++ sut/entrypoint.sh
@@ -12,4 +12,4 @@ service ssh restart;
 
 # The container will run as long as the script is running, that's why
 # we need something long-lived here
-# exec tail -f /var/log/dpkg.log
+exec tail -f /var/log/dpkg.log
