c935774ddaf4ab5628b960f45a2a5a3d6728d913
David Rojo <david-rojo@users.noreply.github.com>
2018-06-28 09:36:34 +0200

    Tomcat log file redirected to docker logs output

--- eim/run.sh
+++ eim/run.sh
@@ -5,4 +5,5 @@ ssh-keygen -q -t rsa -N '' -f /root/.ssh/id_rsa
 
 # The container will run as long as the script is running, that's why
 # we need something long-lived here
-exec tail -f /var/log/tomcat7/catalina.out
+# exec tail -f /var/log/tomcat7/catalina.out
+exec tail -f /var/log/tomcat7/eim-rest-api.log
