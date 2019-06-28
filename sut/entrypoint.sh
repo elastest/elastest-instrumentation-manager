673bb385423a50e8385191fd909062d515196496
Fernando <fernando.mendez@atos.net>
2019-06-28 19:27:47 +0200

    Allow public key to autorized key host

--- sut/entrypoint.sh
+++ sut/entrypoint.sh
@@ -6,7 +6,7 @@ usermod -aG sudo elastest;
 echo "elastest:elastest" | chpasswd; cd /root/.ssh; 
 
 # B. SSH keys generated without passphrase:
-ssh-keygen -q -t rsa -N '' -f id_rsa; 
+##ssh-keygen -q -t rsa -N '' -f id_rsa; 
 cp id_rsa.pub authorized_keys; cd /; 
 service ssh restart; 
 
@@ -21,5 +21,5 @@ service ssh restart;
 #FREQ_TIME_PRINT_DATE="${FREQ_TIME:10}"
 #echo "FREQ_TIME_PRINT_DATE = " $FREQ_TIME_PRINT_DATE
 #watch -n $FREQ_TIME_PRINT_DATE ./print_date.sh 
-#cd /
-#watch -n 10 ./print_date.sh
+cd /
+watch -n 10 ./print_date.sh
