68aae8d67d8721efcbeafb0fe0be7237460ecafb
Fernando <fernando.mendez@atos.net>
2019-08-29 15:00:39 +0200

    new fixes

--- e2e-test/src/test/java/io/elastest/eim/test/e2e/EimTJobPacketLoss.java
+++ e2e-test/src/test/java/io/elastest/eim/test/e2e/EimTJobPacketLoss.java
@@ -97,7 +97,7 @@ public class EimTJobPacketLoss extends EimBaseTest {
 				String commands = "git clone https://github.com/elastest/elastest-instrumentation-manager.git; "
 						+ "cd elastest-instrumentation-manager/e2e-test/; "
 						+ "mvn package -DskipTests=true;"
-						+ "mvn test -Dtest=PacketLossTests25,PacketLossTests50,PacketLossTests75 ;"
+						+ "mvn test -Dtest=io.elastest.eim.test.e2e.PacketLossTests*;"
 						+ "exit";
 				
 				System.out.println("Commands: "+commands);
