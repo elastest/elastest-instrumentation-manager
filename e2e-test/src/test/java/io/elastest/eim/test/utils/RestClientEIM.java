bb9f107b66a77b5950f143c5cbda7d691289817e
Fernando <fernando.mendez@atos.net>
2019-08-30 14:03:10 +0200

    toDo SDK

---
+++ e2e-test/src/test/java/io/elastest/eim/test/utils/RestClientEIM.java
@@ -0,0 +1,18 @@
+package io.elastest.eim.test.utils;
+
+import org.springframework.http.ResponseEntity;
+
+public class RestClientEIM {
+	
+	// To do SDK
+	// Method a) Register Agent
+	// Method b) Unmonitor Agent
+	// Method c) Delete agent
+	// Methodd d) Packetloss Action
+	
+	
+   
+    
+   
+
+}
