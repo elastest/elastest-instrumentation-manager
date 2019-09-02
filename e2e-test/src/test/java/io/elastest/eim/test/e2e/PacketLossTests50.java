ead7de862d8b8c5e98c6958e657596cf808fc51b
Fernando <fernando.mendez@atos.net>
2019-09-02 13:54:51 +0200

    new changes time

--- e2e-test/src/test/java/io/elastest/eim/test/e2e/PacketLossTests50.java
+++ e2e-test/src/test/java/io/elastest/eim/test/e2e/PacketLossTests50.java
@@ -91,8 +91,7 @@ public class PacketLossTests50 {
 			ResponseEntity<String> response = restTemplate.exchange(URL_API,  HttpMethod.GET, request, String.class);
 			System.out.println(response);
 			long elapsedTime = System.nanoTime() - start ;
-			System.out.println("Timing of http request nanoseconds" + elapsedTime);
-			System.out.println("Timing of http request seconds:" + TimeUnit.SECONDS.convert(elapsedTime, TimeUnit.SECONDS));
+			System.out.println("Timing of http request ms:" + TimeUnit.MILLISECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS));
 			responseCode= response.getStatusCode().value();
 
 			
@@ -161,11 +160,9 @@ public class PacketLossTests50 {
 				ResponseEntity<String> response = restTemplate.exchange(URL_API,  HttpMethod.GET, request, String.class);
 				System.out.println(response);
 				long elapsedTime = System.nanoTime() - start ;
-				System.out.println("Timing of http request nanoseconds" + elapsedTime);
-				System.out.println("Timing of http request seconds:" + TimeUnit.SECONDS.convert(elapsedTime, TimeUnit.SECONDS));
+				System.out.println("Timing of http request nanoseconds:" + elapsedTime);
 				// 1 second  = 1_000ms
-				TimeUnit.SECONDS.convert(elapsedTime, TimeUnit.SECONDS);
-				elapesedTimeInMiliSeconds = TimeUnit.SECONDS.convert(elapsedTime, TimeUnit.SECONDS) / 1000.0;
+				elapesedTimeInMiliSeconds = TimeUnit.MILLISECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS);
 				
 				
 			}catch (Exception e) {
