da65f663cf29ee4e9da3bf15246ea256c80b0517
Fernando <fernando.mendez@atos.net>
2019-09-05 14:15:27 +0200

    new json format

--- e2e-test/src/test/java/io/elastest/eim/test/e2e/PacketLossTestSesion.java
+++ e2e-test/src/test/java/io/elastest/eim/test/e2e/PacketLossTestSesion.java
@@ -22,7 +22,7 @@ public class PacketLossTestSesion {
 	private String sut_address = System.getenv("ET_SUT_HOST");
 	private String logstash_ip = System.getenv("ET_MON_LSBEATS_HOST");
 	private String logstash_port = System.getenv("ET_MON_LSBEATS_PORT");
-	private String URL_API = "http://"+sut_address+":"+"5030";
+	private String URL_API = "http://"+sut_address+":"+"5000";
 	private String server = System.getenv("ET_EIM_API")+"/agent/";	
 	private Double latency = 150.0;
 	private String exec_name = System.getenv("ET_MON_EXEC");
